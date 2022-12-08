264ae24f1c2c73259134a2a43d867c736814bb68
= <=>
2022-12-09 00:56:32 +0530

    database connected

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connection");
+require("dotenv").config({ path: "./.env" });
 
 const app = express();
 
@@ -17,4 +19,13 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
