4e376114e0c5c649202e58153d926f694454f7fc
= <=>
2022-12-09 17:45:22 +0530

    trycatch added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,9 +3,14 @@ const Task = require("../models/Task");
 const getAllTasks = (req, res) => {
   res.send("get all tasks");
 };
+
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getTask = (req, res) => {
