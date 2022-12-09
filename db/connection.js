672ceb368a916f55e4ae9dfab4fb14d0617905b3
= <=>
2022-12-09 17:48:52 +0530

    connected to database

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
+  return mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("C O N N E C T E D  T O  D A T A B A S E"))
+    .catch((err) => console.log(err));
 };
 module.exports = connectDB;
-
-// .then(() => console.log("C O N N E C T E D  T O  D A T A B A S E"))
-//   .catch((err) => console.log(err));
