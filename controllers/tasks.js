8699fc8746b295fc894596338d2f26d6eeb2a6dd
= <=>
2022-12-09 18:42:03 +0530

    delete task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -31,12 +31,21 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
+const updateTask = async (req, res) => {
   res.send("update task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID} found` });
+    }
+    res.status(200).json({ task: task, status: "Deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
