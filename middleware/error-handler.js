474dff8d622a36143c28f0cb0ed2c833cc07e735
= <=>
2022-12-13 20:26:28 +0530

    custom error handler

---
+++ middleware/error-handler.js
@@ -0,0 +1,7 @@
+const errorHandlerMiddleware = (err, req, res, next) => {
+  return res
+    .status(500)
+    .json({ msg: `Something went wrong, please try again later` });
+};
+
+module.exports = errorHandlerMiddleware;
